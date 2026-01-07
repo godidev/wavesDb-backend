098b996601cb19087caf1d4cf197f8250ea3d2da
godidev <99983319+godidev@users.noreply.github.com>
2026-01-07 01:02:19 +0100

    fix: correct comparison operator for peakDirection in addMultipleBuoys method

--- src/models/buoy.ts
+++ src/models/buoy.ts
@@ -87,7 +87,7 @@ export class BuoyModel {
         period,
         height,
         avgDirection,
-        ...(peakDirection != undefined && { peakDirection }),
+        ...(peakDirection !== undefined && { peakDirection }),
       }),
     )
 
